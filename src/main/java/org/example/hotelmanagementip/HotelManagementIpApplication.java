87abf59112a1809b8734ae8bd91b8242570ed6e3
Patryk <patryknartowski@wp.pl>
2024-04-30 15:29:16 +0200

    init

---
+++ src/main/java/org/example/hotelmanagementip/HotelManagementIpApplication.java
@@ -0,0 +1,13 @@
+package org.example.hotelmanagementip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementIpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagementIpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/hotelmanagementip/HotelManagementIpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hotelmanagementip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementIpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
