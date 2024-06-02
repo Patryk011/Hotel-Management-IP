9bb2aa58b8aa846856274d88cae601758fbfab17
Patryk <patryknartowski@wp.pl>
2024-06-02 02:14:55 +0200

    factory

--- src/main/java/org/example/hotelmanagementip/services/RoomServiceImpl.java
+++ src/main/java/org/example/hotelmanagementip/services/RoomServiceImpl.java
@@ -3,6 +3,7 @@ package org.example.hotelmanagementip.services;
 import org.example.hotelmanagementip.dto.RoomDTO;
 import org.example.hotelmanagementip.entity.Room;
 import org.example.hotelmanagementip.exception.RoomException;
+import org.example.hotelmanagementip.factory.RoomFactory;
 import org.example.hotelmanagementip.mapper.RoomMapper;
 import org.example.hotelmanagementip.repository.HotelRepository;
 import org.example.hotelmanagementip.repository.RoomRepository;
@@ -13,20 +14,19 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 @Service
-public class RoomServiceImpl implements RoomService{
-
+public class RoomServiceImpl implements RoomService {
 
     private final RoomRepository roomRepository;
-
     private final HotelRepository hotelRepository;
-
     private final RoomMapper roomMapper;
+    private final RoomFactory roomFactory;
 
     @Autowired
-    public RoomServiceImpl(RoomRepository roomRepository, HotelRepository hotelRepository, RoomMapper roomMapper) {
+    public RoomServiceImpl(RoomRepository roomRepository, HotelRepository hotelRepository, RoomMapper roomMapper, RoomFactory roomFactory) {
         this.roomRepository = roomRepository;
         this.hotelRepository = hotelRepository;
         this.roomMapper = roomMapper;
+        this.roomFactory = roomFactory;
     }
 
     @Override
@@ -34,18 +34,16 @@ public class RoomServiceImpl implements RoomService{
         return roomMapper.mapToDto(roomRepository.findAll());
     }
 
-
     @Override
     public List<RoomDTO> getAvailableRooms() {
         return roomMapper.mapToDto(roomRepository.findByIsFree(true));
     }
+
     @Override
     public List<RoomDTO> findByHotelId(Long hotelId) {
         List<Room> rooms = roomRepository.findByHotelId(hotelId);
         return roomMapper.mapToDto(rooms);
     }
-
-
 
     @Override
     public RoomDTO getById(Long id) {
@@ -55,14 +53,12 @@ public class RoomServiceImpl implements RoomService{
 
     @Override
     public RoomDTO addRoom(RoomDTO roomDTO) {
-
         Room existingRoom = roomRepository.findByHotelIdAndNumber(roomDTO.getHotelId(), roomDTO.getNumber());
         if (existingRoom != null) {
-            throw new RoomException("Room with this number " + roomDTO.getNumber() + " exist in this Hotel.");
+            throw new RoomException("Room with this number " + roomDTO.getNumber() + " exists in this Hotel.");
         }
 
-        Room room = roomMapper.mapFromDto(roomDTO);
-        room.setFree(true);
+        Room room = roomFactory.createRoom(roomDTO.getType(), roomDTO.getHotelId(), roomDTO.getNumber(), roomDTO.getPrice());
         room = roomRepository.save(room);
 
         return roomMapper.mapToDto(room);
@@ -81,14 +77,11 @@ public class RoomServiceImpl implements RoomService{
     @Override
     public double getRoomPrice(Long id) {
         Room room = roomRepository.findById(id).orElseThrow(() -> new RoomException("Room with ID " + id + " not found."));
-
         return room.getPrice();
     }
-
 
     @Override
     public void deleteRoom(Long id) {
         roomRepository.deleteById(id);
-
     }
 }
